17e2dbba1022d02919f6177bbec7fc9385c03725
José Antonio Cabrera <joseantoniocabreraalarcon@gmail.com>
2023-05-30 06:51:54 +0200

    carga las imágenes en el showpage

--- mod/collaborate/lib.php
+++ mod/collaborate/lib.php
@@ -433,8 +433,16 @@ function collaborate_pluginfile($course, $cm, $context, $filearea, array $args, 
     }
 
     require_login($course, true, $cm);
+    
+    $fs = get_file_storage();
+    $relativepath = implode('/', $args);
+    $fullpath = "/$context->id/mod_collaborate/$filearea/$relativepath";
+    if (!($file = $fs->get_file_by_hash(sha1($fullpath))) || $file->is_directory()) {
+        return send_file_not_found();
+    }
 
-    send_file_not_found();
+    // Send the file.
+    send_stored_file($file, 0, 0, $forcedownload, $options);
 }
 
 /* Navigation API */
