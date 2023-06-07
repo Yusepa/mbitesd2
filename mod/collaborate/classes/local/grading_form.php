25e370a4718aa22f2e8c17818dc34362dd803b52
José Antonio Cabrera <joseantoniocabreraalarcon@gmail.com>
2023-06-07 07:44:58 +0200

    add grading form

---
+++ mod/collaborate/classes/local/grading_form.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Form for grading.
+ *
+ * @package   mod_collaborate
+ * @copyright 2018 Richard Jones https://richardnz.net
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace mod_collaborate\local;
+defined('MOODLE_INTERNAL') || die();
+require_once($CFG->libdir . '/formslib.php');
+require_once($CFG->libdir . '/filelib.php');
+
+class grading_form extends \moodleform {
+    public function definition() {
+
+        $mform = $this->_form;
+
+        $mform->addElement('hidden', 'cid', $this->_customdata['cid']);
+        $mform->setType('cid', PARAM_INT);
+
+        $mform->addElement('hidden', 'sid', $this->_customdata['sid']);
+        $mform->setType('sid', PARAM_TEXT);
+
+        // Grades available.
+        $grades = array();
+        for ($m = 0; $m <= 100; $m++) {
+            $grades[$m] = $m;
+        }
+
+        $mform->addElement('select', 'grade', 'tx1', $grades);
+        $mform->setType('grade', PARAM_INT);
+
+        // Add a save button.
+        $this->add_action_buttons(false, get_string('submissionsave', 'mod_collaborate'));
+    }
+}
