4b20a3e9175828d0f53b69bea48fd6820f62fde5
José Antonio Cabrera <joseantoniocabreraalarcon@gmail.com>
2023-06-07 07:54:14 +0200

    Add a grading page - grading.php

--- mod/collaborate/db/access.php
+++ mod/collaborate/db/access.php
@@ -77,5 +77,15 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         ),
         'clonepermissionsfrom' => 'moodle/course:manageactivities'
-    )
+    ),
+    'mod/collaborate:gradesubmission' => array(
+        'riskbitmask' => RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
 );
